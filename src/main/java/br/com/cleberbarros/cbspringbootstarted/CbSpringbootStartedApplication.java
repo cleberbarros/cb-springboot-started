f157fd118b903185922b1e3a269b5a7e526f9823
Cleber <cleberbarros.ti@gmail.com>
2021-12-28 23:39:30 -0300

    first commit

---
+++ src/main/java/br/com/cleberbarros/cbspringbootstarted/CbSpringbootStartedApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleberbarros.cbspringbootstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbSpringbootStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbSpringbootStartedApplication.class, args);
+	}
+
+}
